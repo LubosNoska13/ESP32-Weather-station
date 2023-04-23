31a3c050dac151d08f73bb80066785d1f35cd519
Lubos Noska <lubos.noska12@gmail.com>
2023-04-23 20:56:29 +0200

    Add flash messages and first flask forms.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+import credentials
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SECRET_KEY"] = credentials.SECRET_KEY
 
 @app.route("/")
 @app.route("/home")
@@ -10,7 +13,22 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", title="Login")
+    register_form = RegistrationForm()
+    login_form = LoginForm()
+    
+    if login_form.validate_on_submit():
+        flash("You have been login successfully!")
+        print(login_form.email.data)
+        print(login_form.password.data)
+        return redirect(url_for("dashboard"))
+    
+    elif register_form.validate_on_submit():
+        flash("Register")
+        print(register_form.username.data)
+        print(register_form.email.data)
+        return redirect(url_for("dashboard"))
+    return render_template("login.html", title="Login", login_form=login_form, register_form=register_form)
+
 
 @app.route("/dashboard")
 def dashboard():
@@ -19,6 +37,8 @@ def dashboard():
 @app.route("/posts")
 def posts():
     return render_template("posts.html", title="Posts")
+
+
 
 if __name__ == "__main__":
     app.run()
---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Name", validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Username"})
+    email = EmailField("Email", validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=20)], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), Length(min=6, max=20), EqualTo("password")], render_kw={"placeholder": "Confirm"})
+    submit = SubmitField("Sign Up")
+    
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+    
