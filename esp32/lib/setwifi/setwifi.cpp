fb84cd56c8e98b6e4e09f4429d836d43c6c67414
Lubos Noska <lubos.noska12@gmail.com>
2023-06-17 09:53:16 +0200

    Create setwifi.cpp file.

---
+++ esp32/lib/setwifi/setwifi.cpp
@@ -0,0 +1,30 @@
+#include "setwifi.h"
+
+void wifi_setup() {
+    WiFi.mode(WIFI_MODE_STA);
+    WiFi.disconnect(true); // Disconnect from any previously connected network
+}
+
+void scan_network() {
+    Serial.println("Scanning for Wi-Fi networks...");
+    int networkCount = WiFi.scanNetworks();
+    if (networkCount == 0) {
+        Serial.println("No networks found.");
+    } else {
+        Serial.print(networkCount);
+        Serial.println(" networks found.");
+        for (int i = 0; i < networkCount; ++i) {
+            Serial.print("Network name: ");
+            Serial.println(WiFi.SSID(i));
+        }
+    }
+}
+
+void connect_to_wifi(const char* const ssid, const char* const password) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+}
