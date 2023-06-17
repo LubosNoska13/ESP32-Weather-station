aaa7bca0a7f2b7ca11844fc7436460f4e4b2b7a7
Lubos Noska <lubos.noska12@gmail.com>
2023-06-17 09:50:47 +0200

    Create dht.cpp file.

---
+++ esp32/lib/dht/dht.cpp
@@ -0,0 +1,20 @@
+#include "dht.h"
+
+// DHT11 settings
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+static const int DHT_SENSOR_PIN = 4;
+
+DHT_Async dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+bool measure_environment(float *temperature, float *humidity) {
+    static unsigned long measurement_timestamp = millis();
+
+    /* Measure once every four seconds. */
+    if (millis() - measurement_timestamp > 4000ul) {
+        if (dht_sensor.measure(temperature, humidity)) {
+            measurement_timestamp = millis();
+            return true;
+        }
+    }
+    return false;
+}
--- esp32/lib/dht/dht.h
+++ esp32/lib/dht/dht.h
@@ -4,23 +4,8 @@
 // DHT11 (temperature, humidity) library
 #include "DHT_Async.h"
 
-// DHT11 settings
-#define DHT_SENSOR_TYPE DHT_TYPE_11
-static const int DHT_SENSOR_PIN = 4;
+extern DHT_Async dht_sensor;
 
-DHT_Async dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
-
-static bool measure_environment(float *temperature, float *humidity) {
-    static unsigned long measurement_timestamp = millis();
-
-    /* Measure once every four seconds. */
-    if (millis() - measurement_timestamp > 4000ul) {
-        if (dht_sensor.measure(temperature, humidity)) {
-            measurement_timestamp = millis();
-            return (true);
-        }
-    }
-    return (false);
-}
+bool measure_environment(float *temperature, float *humidity);
 
 #endif
