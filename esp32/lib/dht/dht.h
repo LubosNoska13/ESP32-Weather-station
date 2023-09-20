67140cb3cb969f5bcb102395a9da363ee69cb062
Lubos Noska <lubos.noska12@gmail.com>
2023-09-20 09:57:18 +0200

    Rewrite dht.h file in class style (OOP).

--- esp32/lib/dht/dht.h
+++ esp32/lib/dht/dht.h
@@ -1,11 +1,32 @@
 #ifndef DHT_H
 #define DHT_H
 
-// DHT11 (temperature, humidity) library
 #include "DHT_Async.h"
+#include "config.h"
 
-extern DHT_Async dht_sensor;
+/*
+    Class for reading data (temperature and humidity) from the air.
+    Device: DHT11
+*/
 
-bool measure_environment(float *temperature, float *humidity);
+class DhtSensor {
+    private:
+        // Choose your DHT sensor 
+        enum DHT_Type {
+            DHT11,
+            DHT12,
+            DHT21,
+            DHT22
+        };
+        const uint8_t PIN = 4;
+        DHT_Type type = DHT11;
+        DHT_Async dht_sensor;
+        const uint16_t FREQUENCY = frequency_of_reading_data;
+    public:
+        DhtSensor() : dht_sensor(PIN, type) {}
+        // Method to read humidity and temperature
+        bool measure_environment(float *temperature, float *humidity);
+};
 
-#endif
+
+#endif // DHT_H
